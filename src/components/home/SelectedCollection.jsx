2b358c75aad7d89457e86fe8c892af36104a012d
Thinh Pham <thinhpham1842005@gmail.com>
2025-06-24 16:07:25 -0400

    use API to make image appear dynamic

--- src/components/home/SelectedCollection.jsx
+++ src/components/home/SelectedCollection.jsx
@@ -1,44 +1,70 @@
-import React from "react";
-import SelectedItemVideo from "../../assets/selected-collection.mp4";
-import SelectedItemThumbnail from "../../assets/selected-collection-thumbnail.jpg";
-import SelectedItemLogo from "../../assets/selected-collection-logo.avif";
+import React, { useState, useEffect } from "react";
 import VerifiedIcon from "../../assets/verified.png";
 import { Link } from "react-router-dom";
+import axios from "axios";
 
 export default function SelectedCollection() {
+  const [selected, setSelected] = useState({});
+  const [loading, setLoading] = useState(true);
+  async function fetchAPICollection() {
+    try {
+      const { data } = await axios.get(
+        "https://remote-internship-api-production.up.railway.app/selectedCollection"
+      );
+      setSelected(data);
+    } catch (error) {
+      console.log(error);
+    } finally {
+      setLoading(false);
+    }
+  }
+  useEffect(() => {
+    fetchAPICollection();
+    console.log(selected);
+  }, []);
+
   return (
     <header>
       <div className="selected-collection">
-        <video
-          autoPlay
-          muted
-          loop
-          playsInline
-          poster={SelectedItemThumbnail}
-          src={SelectedItemVideo}
-          className="selected-collection__bg"
-        />
-        <div className="selected-collection__description">
-          <img
-            src={SelectedItemLogo}
-            alt=""
-            className="selected-collection__logo"
+        {selected.videoLink && selected.thumbnail && (
+          <video
+            autoPlay
+            muted
+            loop
+            playsInline
+            poster={selected.thumbnail}
+            src={selected.videoLink}
+            className="selected-collection__bg"
           />
-          <h1 className="selected-collection__title">
-            Coachella Throwback Merchandise Trunk
-          </h1>
-          <Link to={'/user'} className="selected-collection__author">
-            By Coachella
+        )}
+        <div className="selected-collection__description">
+          {selected.logo && (
             <img
-              src={VerifiedIcon}
-              className="selected-collection__author__verified"
+              src={selected.logo}
+              alt=""
+              className="selected-collection__logo"
             />
-          </Link>
-          <div className="selected-collection__details">10 items · 3.4 ETH</div>
-          <Link to={'/collection'} className="selected-collection__button">
-            <div className="green-pulse"></div>
-            View Collection
-          </Link>
+          )}
+          <h1 className="selected-collection__title">{selected.title}</h1>
+          {selected.creator && (
+            <Link to={"/user"} className="selected-collection__author">
+              By {selected.creator}
+              <img
+                src={VerifiedIcon}
+                className="selected-collection__author__verified"
+              />
+            </Link>
+          )}
+          {selected.amountOfItems && selected.floorPrice && (
+            <div className="selected-collection__details">{`${selected.amountOfItems} items ${selected.floorPrice} ETH`}</div>
+          )}
+          {selected.collectionId && (
+            <Link to={"/collection"} className="selected-collection__button">
+              <div className="green-pulse">
+                <span className="relative z-10">View Collection</span>
+              </div>
+            </Link>
+          )}
         </div>
       </div>
     </header>
