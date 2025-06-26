d236d642d0b5975ec0347b87ba60d5b4aeeaad0b
Thinh Pham <thinhpham1842005@gmail.com>
2025-06-26 07:54:43 -0400

    complete collection fetch API

--- src/App.jsx
+++ src/App.jsx
@@ -6,20 +6,78 @@ import HomePage from "./pages/HomePage";
 import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
 import ItemPage from "./pages/ItemPage";
 import UserPage from "./pages/UserPage";
+import axios from "axios";
+import { useEffect, useState } from "react";
+
+import { AppContext } from "./context/AppContext";
+import Aos from "aos";
 
 function App() {
+  const [loading, setLoading] = useState(true);
+  const [selected, setSelected] = useState([]);
+  async function fetchAPISelected() {
+    try {
+      const { data } = await axios.get(
+        "https://remote-internship-api-production.up.railway.app/selectedCollection"
+      );
+      setSelected(data.data);
+      setLoading(false);
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  useEffect(() => {
+    fetchAPISelected();
+  }, []);
+
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      Aos.init({
+        disable: false,
+        startEvent: "DOMContentLoaded",
+        initClassName: "aos-init",
+        animatedClassName: "aos-animate",
+        useClassNames: false,
+        disableMutationObserver: false,
+        debounceDelay: 50,
+        throttleDelay: 99,
+        offset: 50,
+        delay: 0,
+        duration: 600,
+        easing: "ease",
+        once: true,
+        mirror: false,
+        anchorPlacement: "top-bottom",
+      });
+      Aos.refreshHard();
+    }, 100);
+
+    return () => clearTimeout(timeout);
+  }, []);
+
+  useEffect(() => {
+    if (!loading) {
+      Aos.refresh();
+    }
+  }, [loading]);
   return (
-    <Router>
-      <Nav />
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/collections" element={<CollectionsPage />} />
-        <Route path="/collection" element={<CollectionPage />} />
-        <Route path="/item" element={<ItemPage />} />
-        <Route path="/user" element={<UserPage />} />
-      </Routes>
-      <Footer />
-    </Router>
+    <AppContext.Provider value={{ loading, selected }}>
+      <Router>
+        <Nav />
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/collections" element={<CollectionsPage />} />
+          <Route
+            path="/collection/:collectionId"
+            element={<CollectionPage />}
+          />
+          <Route path="/item/:id" element={<ItemPage />} />
+          <Route path="/user/:userId" element={<UserPage />} />
+        </Routes>
+        <Footer />
+      </Router>
+    </AppContext.Provider>
   );
 }
 
--- src/components/home/SelectedCollection.jsx
+++ src/components/home/SelectedCollection.jsx
@@ -1,72 +1,61 @@
-import React, { useState, useEffect } from "react";
+import React, { useContext, useEffect } from "react";
 import VerifiedIcon from "../../assets/verified.png";
+
 import { Link } from "react-router-dom";
-import axios from "axios";
+import { AppContext } from "../../context/AppContext";
+import Skeleton from "../ui/Skeleton";
+import Aos from "aos";
 
 export default function SelectedCollection() {
-  const [selected, setSelected] = useState({});
-  const [loading, setLoading] = useState(true);
-  async function fetchAPICollection() {
-    try {
-      const { data } = await axios.get(
-        "https://remote-internship-api-production.up.railway.app/selectedCollection"
-      );
-      setSelected(data);
-    } catch (error) {
-      console.log(error);
-    } finally {
-      setLoading(false);
-    }
-  }
-  useEffect(() => {
-    fetchAPICollection();
-    console.log(selected);
-  }, []);
+  const { selected, loading } = useContext(AppContext);
 
-  return (
+  useEffect(() => {
+    Aos.refresh();
+  }, [selected]);
+
+  return loading !== true ? (
     <header>
       <div className="selected-collection">
-        {selected.videoLink && selected.thumbnail && (
-          <video
-            autoPlay
-            muted
-            loop
-            playsInline
-            poster={selected.thumbnail}
-            src={selected.videoLink}
-            className="selected-collection__bg"
-          />
-        )}
+        <video
+          autoPlay
+          muted
+          loop
+          playsInline
+          poster={selected.thumbnail}
+          src={selected.videoLink}
+          className="selected-collection__bg"
+        />
         <div className="selected-collection__description">
-          {selected.logo && (
-            <img
-              src={selected.logo}
-              alt=""
-              className="selected-collection__logo"
-            />
-          )}
+          <img
+            src={selected.logo}
+            alt=""
+            className="selected-collection__logo"
+          />
           <h1 className="selected-collection__title">{selected.title}</h1>
-          {selected.creator && (
-            <Link to={"/user"} className="selected-collection__author">
-              By {selected.creator}
-              <img
-                src={VerifiedIcon}
-                className="selected-collection__author__verified"
-              />
-            </Link>
-          )}
-          {selected.amountOfItems && selected.floorPrice && (
-            <div className="selected-collection__details">{`${selected.amountOfItems} items ${selected.floorPrice} ETH`}</div>
-          )}
-          {selected.collectionId && (
-            <Link to={"/collection"} className="selected-collection__button">
-              <div className="green-pulse">
-                <span className="relative z-10">View Collection</span>
-              </div>
-            </Link>
-          )}
+          <Link
+            to={`/user/${selected.creatorId}`}
+            className="selected-collection__author"
+          >
+            By {selected.creator}
+            <img
+              src={VerifiedIcon}
+              className="selected-collection__author__verified"
+            />
+          </Link>
+          <div className="selected-collection__details">
+            {selected.amountOfItems} items · {selected.floorPrice} ETH
+          </div>
+          <Link
+            to={`/collection/${selected.collectionId}`}
+            className="selected-collection__button"
+          >
+            <div className="green-pulse"></div>
+            View Collection
+          </Link>
         </div>
       </div>
     </header>
+  ) : (
+    <Skeleton width="100vw" height="50vh" borderRadius="4px" />
   );
 }
